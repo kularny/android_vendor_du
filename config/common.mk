b1fcc314b0142c0f17e6bad1997c5bec52bab685
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2015-09-16 03:23:04 +0530

    Added new improved OTA-Update feature for un-official ROMS

--- config/common.mk
+++ config/common.mk
@@ -149,6 +149,9 @@ PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/du/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
+# OTA Updater
+-include vendor/du/config/common_otaupdater.mk
+
 # Synapse
 -include vendor/du/config/common_synapse.mk
 
---
+++ config/common_otaupdater.mk
@@ -0,0 +1,7 @@
+# OTA Updater
+
+PRODUCT_COPY_FILES += \
+	vendor/du/prebuilt/common/OTA-Updater/OTA-Updater-2.0.5.apk:system/app/OTA-Updater-2.0.5.apk \
+	vendor/du/prebuilt/common/OTA-Updater/rom.ota.prop:system/rom.ota.prop \
+	vendor/du/prebuilt/common/OTA-Updater/ota.prop:system/ota.prop \
+	vendor/du/prebuilt/common/OTA-Updater/com.otaudater.feature.xml:system/etc/permissions/com.otaudater.feature.xml
